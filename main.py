0282793f1b87223318f3cb6f3c38f8d1dc370c93
Jesus Ruiz Palafox <za.17012376@zapopan.tecmm.edu.com>
2020-09-21 17:54:09 -0500

    ajustando espacios

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 def suma():
-   num1 = int(input("Dame un numero:"))
-   num2= int(input("Dame otro numero"))
+   num1 = int(input("Dame un numero: "))
+   num2= int(input("Dame otro numero 1"))
    print("la suma es", num1 + num2)
 
 if __name__ == '__main__':
